2291c0b149efb7309137216125c9b2013310a923
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-24 01:49:51 -0400

    Refactor

--- src/app/main.go
+++ src/app/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
 	resthandler "handler/rest"
 )
 
 func main() {
-	fmt.Println("Janus Initialization")
+	initializeJanus()
+}
 
+//Janus Initialization
+func initializeJanus() {
+	//Initialize REST Handler
 	resthandler.LaunchRestHandler()
 }
