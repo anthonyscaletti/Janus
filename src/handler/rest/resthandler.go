f0020736b142f78953f1a4f4aa5e80689b3742b2
Tony Scaletti <ascaletti@shoplogix.com>
2020-04-24 00:40:54 -0400

    Created API entry handler

--- src/handler/rest/resthandler.go
+++ src/handler/rest/resthandler.go
@@ -2,9 +2,20 @@ package resthandler
 
 import (
 	"fmt"
+	"net/http"
 )
 
 //LaunchRestHandler : REST Handler Initialization
 func LaunchRestHandler() {
-	fmt.Println("Launching Rest Handler")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", EntryHandler)
+
+	fmt.Println("Listening...")
+	http.ListenAndServe(":5000", mux)
+}
+
+//EntryHandler : API Entry Page
+func EntryHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "API Entry Page")
 }
